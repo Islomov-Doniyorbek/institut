a5f99afcbf85799087f934b72f1607346401bc3b
Islomov-Doniyorbek <islomovdoniyorbey@gmail.com>
2025-07-20 20:02:26 +0500

    Layout

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Layout from './layout'
+
+function App() {
+  return (
+    
+    <Layout>
+      <div>Page</div>
+    </Layout>
+  )
+}
+
+export default App
---
+++ src/layout.jsx
@@ -0,0 +1,26 @@
+import { MdLocationOn, MdPhone } from 'react-icons/md';
+
+const Layout = ({ children }) => {
+  return (
+    <div className='container'>
+      <header>
+        <div className="topBar">
+          <p>
+            <span><MdLocationOn /></span>  Toshkent 100027, Botir Zokirov ko'chasi 1 uy
+          </p>
+          <p><span><MdPhone /></span> +998 77 200 00 00</p>
+        </div>
+        <nav className="nav">
+
+        </nav>
+      </header>
+      <main>{children}</main>
+      <footer>
+        
+        Footer
+      </footer>
+    </div>
+  );
+};
+
+export default Layout;
