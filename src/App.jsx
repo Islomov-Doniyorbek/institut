10e0722728521552df57571378fa43ca6bbae103
Farrukhjon Majitov <farruxmajidov10@gmail.com>
2025-07-26 17:18:48 +0500

    created bakalavr page

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,52 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Komponents from './Main/Komponents'
-import Activity from './Pages/Activity/Activity'
-import ActiveCouncil from './Pages/Activity/ActiveCouncil'
-import Article from './Pages/Activity/Article'
-import Projects from './Pages/Activity/Projects'
-import Tuzilma from './Pages/Tuzilma/Tuzilma'
-import './App.css'
-import './Main.css'
-import Container from './components/Container'
-import FakultetInfo from './Pages/Tuzilma/fakultetInfo'
-import Allnews from './Pages/InfoCenter/Allnews'
-import AllnewsInner from './Pages/InfoCenter/AllnewsInner'
-import Event from './Pages/InfoCenter/Event'
-import Galery from './Pages/InfoCenter/Galery'
-import OtherNews from './Pages/InfoCenter/Othernews'
-import Layout from './layout'
-import Connect from './Pages/InfoCenter/Connect'
-import Documents from './Pages/InfoCenter/Documents'
-import FakultetKafedra from './Pages/Tuzilma/fakultet'
+import Komponents from "./Main/Komponents";
+import Activity from "./Pages/Activity/Activity";
+import ActiveCouncil from "./Pages/Activity/ActiveCouncil";
+import Article from "./Pages/Activity/Article";
+import Projects from "./Pages/Activity/Projects";
+import Tuzilma from "./Pages/Tuzilma/Tuzilma";
+import "./App.css";
+import "./Main.css";
+import Container from "./components/Container";
+import FakultetInfo from "./Pages/Tuzilma/fakultetInfo";
+import Allnews from "./Pages/InfoCenter/Allnews";
+import AllnewsInner from "./Pages/InfoCenter/AllnewsInner";
+import Event from "./Pages/InfoCenter/Event";
+import Galery from "./Pages/InfoCenter/Galery";
+import OtherNews from "./Pages/InfoCenter/Othernews";
+import Layout from "./layout";
+import Connect from "./Pages/InfoCenter/Connect";
+import Documents from "./Pages/InfoCenter/Documents";
+import FakultetKafedra from "./Pages/Tuzilma/fakultet";
+import Bakalavr from "./Pages/Talim/Bakalavr";
 
 function App() {
   return (
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Komponents />} />
-          <Route path="Activity/Activity" element={<Activity />} />
-          <Route path="Activity/ActiveCouncil" element={<ActiveCouncil />} />
-          <Route path="Activity/Article" element={<Article />} />
-          <Route path="Activity/Projects" element={<Projects />} />
-          <Route path="/Tuzilma/fakultet" element={<FakultetKafedra />} />
-          <Route path="/Tuzilma/fakultetInfo/:fakultet" element={<FakultetInfo />} />
-          <Route path="/Tuzilma/Tuzilma" element={<Tuzilma />} />
-          <Route path="/News/Allnews" element={<Allnews />} />
-          <Route path="/News/Events" element={<Event />} />
-          <Route path="/News/Galery" element={<Galery />} />
-          <Route path="/News/Connect" element={<Connect />} />
-          <Route path="/News/Documents" element={<Documents />} />
-          <Route path="/News/Allnews/Innernews" element={<AllnewsInner/>} />
-          <Route path="/News/Allnews/Othernews" element={<OtherNews/>} />
-        </Route>
-      </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Komponents />} />
+        <Route path="Activity/Activity" element={<Activity />} />
+        <Route path="Activity/ActiveCouncil" element={<ActiveCouncil />} />
+        <Route path="Activity/Article" element={<Article />} />
+        <Route path="Activity/Projects" element={<Projects />} />
+        <Route path="/Tuzilma/fakultet" element={<FakultetKafedra />} />
+        <Route
+          path="/Tuzilma/fakultetInfo/:fakultet"
+          element={<FakultetInfo />}
+        />
+        <Route path="/Tuzilma/Tuzilma" element={<Tuzilma />} />
+        <Route path="/News/Allnews" element={<Allnews />} />
+        <Route path="/News/Events" element={<Event />} />
+        <Route path="/News/Galery" element={<Galery />} />
+        <Route path="/News/Connect" element={<Connect />} />
+        <Route path="/News/Documents" element={<Documents />} />
+        <Route path="/News/Allnews/Innernews" element={<AllnewsInner />} />
+        <Route path="/News/Allnews/Othernews" element={<OtherNews />} />
+        <Route path="/Talim/Bakalavr" element={<Bakalavr />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/layout.jsx
+++ src/layout.jsx
@@ -1,160 +1,187 @@
 // import { MdLocationOn, MdPhone } from 'react-icons/md';
-import Footer from './Main/Footer';
-import { MdArrowCircleDown, MdArrowDropDown, MdClose, MdLocationOn, MdMenu, MdPhone } from 'react-icons/md';
-import Logo from './assets/imgs/logo-estrada.png'
-import { useState } from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import Nav from './components/Nav';
-
+import Footer from "./Main/Footer";
+import {
+  MdArrowCircleDown,
+  MdArrowDropDown,
+  MdClose,
+  MdLocationOn,
+  MdMenu,
+  MdPhone,
+} from "react-icons/md";
+import Logo from "./assets/imgs/logo-estrada.png";
+import { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+import Nav from "./components/Nav";
 
 const Layout = () => {
-    const navLinks = [
-        {
-            id: 0,
-            linkName: {link:"Bosh sahifa", path: '/'},
-            linkList: null
-        },
-        {
-            id: 1,
-            linkName: "Institut",
-            
-            linkList: [
-                {
-                    nestedLinkName: "Tuzilma",
-                    nestedLinks: [
-                        {link:"Institut tuzilmasi", path: '/Tuzilma/Tuzilma', slug: "Institut tuzilmasi"},
-                        {link:"Tarkibiy tuzilma", path: '/Activity/Activity', slug: "Institut tuzilmasi"},
-                        {link:"Fakultetlar va kafedralar", path: '/Tuzilma/fakultet'}, 
-                    ]
-                },
-                [
-                {link:"Rahbariyat", path: '/Activity/Activity'},
-                {link:"Institut tarixi", path: '/Activity/Activity'},
-                {link:"Me'yoriy-huquqiy hujjatlar", path: '/Activity/Activity'},
-                {link:"Xalqaro hamkorlik", path: '/Activity/Activity'},
-                {link:"Foydali havolalar", path: '/Activity/Activity'},
-                {link:"Botir Zokirov hayoti va ijodi", path: '/Activity/Activity'},
-                ]
-            ]
-        },
-        {
-            id: 2,
-            linkName: "Ta'lim",
-            linkList: [
-                [
-                {link:"Bakalavr", path: '/Activity/Activity'},
-                {link:"Magistratura", path: '/Activity/Activity'},
-                {link:"Dars jadvali", path: '/Activity/Activity'},
-                {link:"Bitiruvchilar", path: '/Activity/Activity'},
-            ]
-        ]
+  const navLinks = [
+    {
+      id: 0,
+      linkName: { link: "Bosh sahifa", path: "/" },
+      linkList: null,
     },
     {
-        id: 3,
-        linkName: "Ilmiy faoliyat",
-        linkList: [
-            [
-                {link:"Ilmiy tadqiqot faoliyati", path: '/Activity/Activity'},
-                {link:"Ilmiy kengashlar", path: '/Activity/ActiveCouncil'},
-                {link:"Ilmiy jurnallar", path: '/Activity/Article'},
-                {link:"Ilmiy loyihalar", path: '/Activity/Projects'},
-            ]
-            ]
-        },
-        {
-            id: 4,
-            linkName: "Axborot xizmati",
-            linkList: [
-                [
-                {link:"Yangiliklar", path: '/News/Allnews'},
-                {link:"Tadbirlar", path: '/News/Events'},
-                {link:"Media galereyalar", path: '/News/Galery'},
-                {link:"Bog'lanish", path: '/News/Connect'},
-                {link:"Qabul 2025-natijalari", path: '/News/Documents'},
-            ]
-            ]
-        },
-        {
-            id: 5,
-            linkName: "Qabul-2025",
-            linkList: [
-                [
-                    {link:"Qabul \"Call Centre\"", path: '/Activity/Activity'},
-                    {link:"Qabul kvotalari (BA & MA)", path: '/Activity/Activity'},
-                    {link:"Xorijiy fuqarolar uchun qabul", path: '/Activity/Activity'},
-                    {link:"Qabul dasturlari", path: '/Activity/Activity'},
-                    {link:"Kasbiy(Ijodiy) imtihonlar translatsiyasi", path: '/Activity/Activity'},
-                ]
-            ]
-        },
-        {
-            id: 6,
-            linkName: "Talabalar",
-            linkList: [
-                [
-                    {link:"Ekofaol talabalar", path: '/Activity/Activity'},
-                    {link:"Grant uchun ariza", path: '/Activity/Activity'},
-                    {link:"Talaba yutuqlarini jo'natish", path: '/Activity/Activity'},
-                    {link:"Grant uchun test", path: '/Activity/Activity'},
-                    {link:"Kasbiy(Ijodiy) imtihonlar translatsiyasi", path: '/Activity/Activity'},
-                ]
-            ]
-        },
-        {
-            id: 7,
-            linkName: "Syurpriz",
-            linkList: null
-        },
-    ]
+      id: 1,
+      linkName: "Institut",
 
-    console.log(navLinks[1].linkList[0].nestedLinks);
-    
-    const [menu, setMenu] = useState(true)
+      linkList: [
+        {
+          nestedLinkName: "Tuzilma",
+          nestedLinks: [
+            {
+              link: "Institut tuzilmasi",
+              path: "/Tuzilma/Tuzilma",
+              slug: "Institut tuzilmasi",
+            },
+            {
+              link: "Tarkibiy tuzilma",
+              path: "/Activity/Activity",
+              slug: "Institut tuzilmasi",
+            },
+            { link: "Fakultetlar va kafedralar", path: "/Tuzilma/fakultet" },
+          ],
+        },
+        [
+          { link: "Rahbariyat", path: "/Activity/Activity" },
+          { link: "Institut tarixi", path: "/Activity/Activity" },
+          { link: "Me'yoriy-huquqiy hujjatlar", path: "/Activity/Activity" },
+          { link: "Xalqaro hamkorlik", path: "/Activity/Activity" },
+          { link: "Foydali havolalar", path: "/Activity/Activity" },
+          { link: "Botir Zokirov hayoti va ijodi", path: "/Activity/Activity" },
+        ],
+      ],
+    },
+    {
+      id: 2,
+      linkName: "Ta'lim",
+      linkList: [
+        [
+          { link: "Bakalavr", path: "/Talim/Bakalavr" },
+          { link: "Magistratura", path: "/Activity/Activity" },
+          { link: "Dars jadvali", path: "/Activity/Activity" },
+          { link: "Bitiruvchilar", path: "/Activity/Activity" },
+        ],
+      ],
+    },
+    {
+      id: 3,
+      linkName: "Ilmiy faoliyat",
+      linkList: [
+        [
+          { link: "Ilmiy tadqiqot faoliyati", path: "/Activity/Activity" },
+          { link: "Ilmiy kengashlar", path: "/Activity/ActiveCouncil" },
+          { link: "Ilmiy jurnallar", path: "/Activity/Article" },
+          { link: "Ilmiy loyihalar", path: "/Activity/Projects" },
+        ],
+      ],
+    },
+    {
+      id: 4,
+      linkName: "Axborot xizmati",
+      linkList: [
+        [
+          { link: "Yangiliklar", path: "/News/Allnews" },
+          { link: "Tadbirlar", path: "/News/Events" },
+          { link: "Media galereyalar", path: "/News/Galery" },
+          { link: "Bog'lanish", path: "/News/Connect" },
+          { link: "Qabul 2025-natijalari", path: "/News/Documents" },
+        ],
+      ],
+    },
+    {
+      id: 5,
+      linkName: "Qabul-2025",
+      linkList: [
+        [
+          { link: 'Qabul "Call Centre"', path: "/Activity/Activity" },
+          { link: "Qabul kvotalari (BA & MA)", path: "/Activity/Activity" },
+          { link: "Xorijiy fuqarolar uchun qabul", path: "/Activity/Activity" },
+          { link: "Qabul dasturlari", path: "/Activity/Activity" },
+          {
+            link: "Kasbiy(Ijodiy) imtihonlar translatsiyasi",
+            path: "/Activity/Activity",
+          },
+        ],
+      ],
+    },
+    {
+      id: 6,
+      linkName: "Talabalar",
+      linkList: [
+        [
+          { link: "Ekofaol talabalar", path: "/Activity/Activity" },
+          { link: "Grant uchun ariza", path: "/Activity/Activity" },
+          { link: "Talaba yutuqlarini jo'natish", path: "/Activity/Activity" },
+          { link: "Grant uchun test", path: "/Activity/Activity" },
+          {
+            link: "Kasbiy(Ijodiy) imtihonlar translatsiyasi",
+            path: "/Activity/Activity",
+          },
+        ],
+      ],
+    },
+    {
+      id: 7,
+      linkName: "Syurpriz",
+      linkList: null,
+    },
+  ];
 
-    const menubar = bool => {
-        setMenu(bool)
-    }
+  console.log(navLinks[1].linkList[0].nestedLinks);
+
+  const [menu, setMenu] = useState(true);
+
+  const menubar = (bool) => {
+    setMenu(bool);
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <header>
         <div className="topBar">
           <p>
-            <span><MdLocationOn /></span>  Toshkent 100027, Botir Zokirov ko'chasi 1 uy
+            <span>
+              <MdLocationOn />
+            </span>{" "}
+            Toshkent 100027, Botir Zokirov ko'chasi 1 uy
           </p>
-          <p><span><MdPhone /></span> +998 77 200 00 00</p>
+          <p>
+            <span>
+              <MdPhone />
+            </span>{" "}
+            +998 77 200 00 00
+          </p>
         </div>
         <nav className="nav">
-            <div className="col">
-                <div className="logo">
-                    <img src={Logo} alt="" />
-                    <h2>O'zbekiston Davlat KOnservatoriyasi huzuridagi <br/>
-                        Botir Zokirov nomidagi <br/>
-                        Milliy Estrada san'ati instituti
-                    </h2>
-                </div>
+          <div className="col">
+            <div className="logo">
+              <img src={Logo} alt="" />
+              <h2>
+                O'zbekiston Davlat KOnservatoriyasi huzuridagi <br />
+                Botir Zokirov nomidagi <br />
+                Milliy Estrada san'ati instituti
+              </h2>
             </div>
-            <div className="col">
-                <div className="menuBar">
-                   <span className='menuIcon' onClick={() => menubar(false)}>
-                    <MdMenu />
-                    </span>
-                    <div className={`sideBarMenu ${menu ? "hide" : "scr"}`}>
-                            <span className='close' onClick={() => menubar(true)}>
-                                <MdClose />
-                            </span>
-                            <ul className="navLinks">
-                                <Nav arr={navLinks} />
-                            </ul>
-                    </div>
-                </div>
-                <ul className="navLinks rwdNav">
-                    <Nav arr={navLinks} />                    
+          </div>
+          <div className="col">
+            <div className="menuBar">
+              <span className="menuIcon" onClick={() => menubar(false)}>
+                <MdMenu />
+              </span>
+              <div className={`sideBarMenu ${menu ? "hide" : "scr"}`}>
+                <span className="close" onClick={() => menubar(true)}>
+                  <MdClose />
+                </span>
+                <ul className="navLinks">
+                  <Nav arr={navLinks} />
                 </ul>
-                <div className="menuDetails">
-                    detal
-                </div>
+              </div>
             </div>
+            <ul className="navLinks rwdNav">
+              <Nav arr={navLinks} />
+            </ul>
+            <div className="menuDetails">detal</div>
+          </div>
         </nav>
       </header>
       <main>
@@ -162,7 +189,7 @@ const Layout = () => {
       </main>
       {/* shu childrenga Komponents keladi, avtomatik layout o'zgarmay tururadi. */}
       <footer>
-        <Footer/>
+        <Footer />
       </footer>
     </div>
   );
