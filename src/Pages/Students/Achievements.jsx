5dfab56d2006a9bee6c0bedd5549d7b26db988bf
Ibrohim39 <ibrohimismoiloff07@gmail.com>
2025-08-21 14:23:24 +0500

    jonatish

--- src/Pages/Students/Achievements.jsx
+++ src/Pages/Students/Achievements.jsx
@@ -1,13 +1,44 @@
 import React from 'react'
 import ActiveTopPage from '../../components/activeTopPage'
 import { useTranslation } from 'react-i18next'
-import NotFoundPage from '../../components/404'
+
 const Achievements = () => {
    const {t}=useTranslation()
   return (
     <div className='achiv'>
            <ActiveTopPage pageTitle={t("send_achievements")} />
-      <NotFoundPage/>
+<div className='achive_send'>
+  <h2></h2>
+  <div className='achive_send_row'>
+<div className='achive_send_card'>
+<h1>O'quvchining ism familiyasi</h1>
+<input type='text' placeholder='ism kiriting'/>
+</div>
+<div className='achive_send_card'>
+<h1>Fakultetingizni kiriting</h1>
+<input type='text' placeholder='fakultet'/>
+</div>
+<div className='achive_send_card'>
+  <h1>Kursingizni kiriting</h1>
+  <input 
+    type="number" 
+    placeholder="kurs" 
+    min="1" 
+    max="4"
+  />
+</div>
+<div className='achive_send_card'>
+  <h1>Yutug'ingizni pdf shaklda yuboring</h1>
+  <input 
+    type="file" 
+    accept="application/pdf" 
+  />
+</div>
+<div className='achive_send_card'>
+<button>Yuborish</button>
+</div>
+</div>
+</div>
     </div>
   )
 }
