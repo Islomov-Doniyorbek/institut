f2b8aba8a78624cc03b2637b4a97eebfc0f0804d
Ibrohim39 <ibrohimismoiloff07@gmail.com>
2025-08-23 17:53:11 +0500

    fixed

--- src/Main/News.jsx
+++ src/Main/News.jsx
@@ -31,7 +31,7 @@ const onChange =()=>{
       </div>
       
             <div className='news_card'> 
-<div className='news_img news_img_stat2'>
+<div className='news_img '>
  </div>
 <div className='news_text'>
 <h2>2025/2026 o‘quv yili 2-kursi uchun qayta taqsimlanadigan ta’lim grantlari o‘rinlari e’lon qilindi! 
@@ -45,7 +45,7 @@ const onChange =()=>{
        
        
                  <div className='news_card'> 
-<div className='news_img news_img_stat3'>
+<div className='news_img '>
  </div>
 <div className='news_text'>
 <h2>“Anti- korrupsiya” bilimdoni bilimlar bellashuvi bo‘lib o‘tdi
@@ -59,7 +59,7 @@ const onChange =()=>{
 
 
               <div className='news_card'> 
-<div className='news_img news_img_stat4'>
+<div className='news_img '>
  </div>
 <div className='news_text'>
 <h2>Hamid Olimjon tavalludining 115-yilligiga bag‘ishlangan anjuman doirasidagi konsert dasturi
